1796f39331a8f2b50955a3539152d0fa159345a0
DavidSchuchert <Davidschuchert@icloud.com>
2024-01-25 17:47:56 +0100

    commit

---
+++ src/HeaderTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace HeaderTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class HeaderTheme extends Plugin implements ThemeInterface
+{
+}
---
+++ src/Resources/app/storefront/dist/storefront/js/header-theme.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["header-theme"],{
+
+/***/ "TD/E":
+/*!*****************************************************************************************!*\
+  !*** /var/www/html/custom/plugins/HeaderTheme/src/Resources/app/storefront/src/main.js ***!
+  \*****************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVEQvRS5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///TD/E\n");
+
+/***/ })
+
+},[["TD/E","runtime"]]]);
